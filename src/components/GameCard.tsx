fcef919b02cba2ac3c4f1abb16dbe466eda368a5
Mahid Ahmad <mahid797@hotmail.com>
2024-02-04 20:20:19 -0500

    Building Game Cards

---
+++ src/components/GameCard.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Game } from '../hooks/useGames';
+import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
+import PlatformIconList from './PlatformIconList';
+import getCroppedImageUrl from '../services/image-url';
+import CriticScore from './CriticScore';
+
+interface Props {
+	game: Game;
+}
+
+const GameCard = ({ game }: Props) => {
+	return (
+		<Card borderRadius={10} overflow="hidden">
+			<Image src={getCroppedImageUrl(game.background_image)} />
+			<CardBody>
+				<Heading fontSize="2xl">{game.name}</Heading>
+				<HStack justifyContent="space-between">
+					<PlatformIconList
+						platforms={game.parent_platforms.map(
+							(p) => p.platform
+						)}></PlatformIconList>
+					<CriticScore score={game.metacritic} />
+				</HStack>
+			</CardBody>
+		</Card>
+	);
+};
+
+export default GameCard;
